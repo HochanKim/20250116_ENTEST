e56be287658d7d18aa185075ea5e88955069fc2a
Hochan Kim <ghckskim@gmail.com>
2025-01-16 22:06:17 +0900

    2025.01.16 react, tailwindCSS 설치치

---
+++ test/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className='text-red-500'>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1 className='text-teal-800 uppercase'>hello, world</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
